9c6438e16831ca46447f1f2c3a79c3d894270ba2
sangwon38383 <54392189+sangwon38383@users.noreply.github.com>
2021-04-05 01:00:50 +0900

    Update urls.py

--- bonds/urls.py
+++ bonds/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.bond_list, name='bond_list'), 
+    path('def', views.default_bond_list, name = 'default_bond_list'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.bond_detail, name='bond_detail'),
     path('post/<int:pk>/sell/', views.bond_sell, name='bond_sell'),
